462a3e2591feb5282b4e7e116689c2971023ce4c
Eike Frota <eikefrotaa@hotmail.com>
2025-04-07 19:59:43 -0300

    Introdução a conexão com o Banco de Dados

---
+++ UC7/banco-de-dados/db.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import dotenv from 'dotenv';
+import pg from "pg";
+
+dotenv.config();
+
+let pool
+
+const getConnection = () => {
+    if (!pool) {
+        pool = new pg.Pool({
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            database: process.env.DB_NAME,
+            ssl: {
+                rejectUnauthorized: true,
+                ca: fs.readFileSync("./ca.pem").toString(),
+            }
+        });
+    }
+
+    return pool;
+}
+
+const closeConnection = async () => {
+    if (pool) {
+        await pool.end();
+        pool = null;
+    }
+}
