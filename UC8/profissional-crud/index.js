64a227b0a322a556faaf96df2b1690cd2445bcef
Eike Frota <eikefrotaa@hotmail.com>
2025-05-05 18:52:46 -0300

    Excluindo celular

--- UC8/profissional-crud/index.js
+++ UC8/profissional-crud/index.js
@@ -6,12 +6,10 @@ app.use(morgan('dev')); // Loga no terminal
 require('dotenv').config();
 
 const profissionalRoutes = require('./routes/profissionalRoutes');
-const celularRoutes = require('./routes/celularRoutes');
 const errorMiddleware = require('./middleware/errorMiddleware');
 
 app.use(express.json());
 app.use('/profissionais', profissionalRoutes);
-app.use('/celulares', celularRoutes);
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
--- UC8/profissional-crud/middleware/validateCelular.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-    const { nome, valor, ano_fabricacao, nome_dono } = req.body;
-  
-    if (!nome || !valor || !ano_fabricacao || !nome_dono) {
-        return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
-    }
-    
-    next();
-};
