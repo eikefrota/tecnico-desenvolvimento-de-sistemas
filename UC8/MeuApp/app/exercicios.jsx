0fece4172fd13116cab07f91af837e7bf9661d99
Eike Frota <eikefrotaa@hotmail.com>
2025-05-13 20:39:03 -0300

    Exercicios Hooks

---
+++ UC8/MeuApp/app/exercicios.jsx
@@ -0,0 +1,6 @@
+import { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+
+export default function contador(){
+    const [contador, setContador] = useState(0);
+};
--- UC8/MeuApp/app/index.jsx
+++ UC8/MeuApp/app/index.jsx
@@ -1,135 +1,82 @@
-import { useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
+// ----------------------------useContext---------------------------------------------
+// import React, { useContext, useState, createContext } from 'react';
+// import { View, Text, Button } from 'react-native';
 
-// export default function multiplicacao(){
-//   const [valor1, setValor1] = useState("");
-//   const [valor2, setValor2] = useState("")
-//   const resultado = Number(valor1) * Number(valor2);
+// const TemaContexto = createContext();
 
-//   return(
-//     <View style={{flex: 1, justifyContent: "center", alignItems: "center", gap: 50}}>
-//       <TextInput placeholder='Digite o valor 1:' keyboardType='numeric' onChangeText={setValor1} value={valor1}></TextInput>
-
-//       <TextInput placeholder='Digite o valor 2:' keyboardType='numeric' onChangeText={setValor2} value={valor2}></TextInput>
-
-//       <Text>Resultado: {resultado}</Text>
-//     </View>
-//   )
+// function TemaProvider({ children }) {
+//   const [tema, setTema] = useState('claro');
+//   return (
+//     <TemaContexto.Provider value={{ tema, setTema }}>
+//       {children}
+//     </TemaContexto.Provider>
+//   );
 // }
 
-// export default function divisao(){
-//   const [valor1, setValor1] = useState("");
-//   const [valor2, setValor2] = useState("");
-//   const resultado = Number(valor1) / Number(valor2);
-
-//   return(
-//     <View style={{flex: 1, justifyContent: "center", alignItems: "center", gap: 50}}>
-//       <TextInput placeholder='Digite o valor 1:' keyboardType='numeric' onChangeText={setValor1} value={valor1}></TextInput>
-
-//       <TextInput placeholder='Digite o valor 2:' keyboardType='numeric' onChangeText={setValor2} value={valor2}></TextInput>
-
-//       <Text>Resultado: {resultado}</Text>
+// function MostrarTema() {
+//   const { tema, setTema } = useContext(TemaContexto);
+//   return (
+//     <View>
+//       <Text>Tema: {tema}</Text>
+//       <Button
+//         title="Alternar Tema"
+//         onPress={() => setTema(tema === 'claro' ? 'escuro' : 'claro')}
+//       />
 //     </View>
-//   )
+//   );
 // }
 
-
-
-// export default function potencia(){
-//   const [valor1, setValor1] = useState("");
-//   const [valor2, setValor2] = useState("");
-//   const resultado = Number(valor1) ** Number(valor2);
-
-//   return(
-//     <View style={{flex: 1, justifyContent: "center", alignItems: "center", gap: 50}}>
-//       <TextInput placeholder='Digite o valor 1:' keyboardType='numeric' onChangeText={setValor1} value={valor1}></TextInput>
-
-//       <TextInput placeholder='Digite o valor 2:' keyboardType='numeric' onChangeText={setValor2} value={valor2}></TextInput>
-
-//       <Text>Resultado: {resultado}</Text>
-//     </View>
-//   )
+// export default function App() {
+//   return (
+//     <TemaProvider>
+//       <MostrarTema />
+//     </TemaProvider>
+//   );
 // }
 
 
+//-------------------------------useRef----------------------------------------------------
+// import { useRef } from 'react';
+// import { Button, TextInput, View } from 'react-native';
 
-// export default function raizQuadrada(){
-//   const [valor, setValor] = useState("");
-//   const resultado = Math.sqrt(valor)
+// export default function FocoNoInput() {
+//   const inputRef = useRef(null);
 
-//   return(
-//     <View style={{flex: 1, justifyContent: "center", alignItems: "center", gap: 50}}>
-//       <TextInput placeholder='Digite o valor:' keyboardType='numeric' onChangeText={setValor} value={valor}></TextInput>
-
-//       <Text>Resultado: {resultado}</Text>
+//   return (
+//     <View>
+//       <TextInput ref={inputRef} placeholder="Digite aqui" />
+//       <Button title="Focar no campo" onPress={() => inputRef.current.focus()} />
 //     </View>
-//   )
+//   );
 // }
 
 
 
-// export default function media(){
-//   const [valor1, setValor1] = useState("");
-//   const [valor2, setValor2] = useState("");
-//   const resultado = (Number(valor1) + Number(valor2)) / 2;
+// ---------------------------------useCallback-------------------------------------------
+import { useCallback, useState } from 'react';
+import { Button, Text, View } from 'react-native';
 
-//   return(
-//     <View style={{flex: 1, justifyContent: "center", alignItems: "center", gap: 50}}>
-//       <TextInput placeholder='Digite o valor 1:' keyboardType='numeric' onChangeText={setValor1} value={valor1}></TextInput>
+export default function BotaoMemoizado() {
+  const [cont, setCont] = useState(0);
 
-//       <TextInput placeholder='Digite o valor 2:' keyboardType='numeric' onChangeText={setValor2} value={valor2}></TextInput>
+  const incrementar = useCallback(() => {
+    setCont(prev => prev + 1);
+  }, []);
 
-//       <Text>Resultado: {resultado}</Text>
-//     </View>
-//   )
-// }
+  const decrementar = useCallback(() => {
+    setCont(prev => prev - 1);
+  }, []);
 
+  const resetar = useCallback(() => {
+    setCont(prev => 0);
+  }, []);
 
-// export default function restoDaDivisao(){
-//   const [valor1, setValor1] = useState("");
-//   const [valor2, setValor2] = useState("");
-//   const resultado = Number(valor1) % Number(valor2);
-
-//   return(
-//     <View style={{flex: 1, justifyContent: "center", alignItems: "center", gap: 50}}>
-//       <TextInput placeholder='Digite o valor 1:' keyboardType='numeric' onChangeText={setValor1} value={valor1}></TextInput>
-
-//       <TextInput placeholder='Digite o valor 2:' keyboardType='numeric' onChangeText={setValor2} value={valor2}></TextInput>
-
-//       <Text>Resultado: {resultado}</Text>
-//     </View>
-//   )
-// }
-
-
-
-// export default function dobro(){
-//   const [valor, setValor] = useState("");
-//   const resultado = valor * 2
-
-//   return(
-//     <View style={{flex: 1, justifyContent: "center", alignItems: "center", gap: 50}}>
-//       <TextInput placeholder='Digite o valor:' keyboardType='numeric' onChangeText={setValor} value={valor}></TextInput>
-
-//       <Text>Resultado: {resultado}</Text>
-//     </View>
-//   )
-// }
-
-
-
-export default function imparPar(){
-  const [valor, setValor] = useState("");
-  const resto = Number(valor) % 2;
-  let resultado = "" 
-
-  if (resto == 0 ? resultado= "Par" : resultado = "Ímpar")
-
-  return(
-    <View style={{flex: 1, justifyContent: "center", alignItems: "center", gap: 50}}>
-      <TextInput placeholder='Digite o valor 1:' keyboardType='numeric' onChangeText={setValor} value={valor}></TextInput>
-
-      <Text>Resultado: {resultado}</Text>
+  return (
+    <View>
+      <Text>Contador: {cont}</Text>
+      <Button title="Incrementar" onPress={incrementar} />
+      <Button title="Decrementar" onPress={decrementar} />
+      <Button title="Resetar" onPress={resetar} />
     </View>
-  )
+  );
 }
